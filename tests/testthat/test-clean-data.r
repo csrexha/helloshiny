3f3f0ea16f90d3e44270c2bbd77326cb9ab61dc7
HaCSRex <hachungshingrex@gmail.com>
2026-06-15 00:51:35 +0200

    test: correct test_that syntax in test-clean-data.r and remove "fte_quality" from expected_colums

--- tests/testthat/test-clean-data.r
+++ tests/testthat/test-clean-data.r
@@ -1,17 +1,16 @@
 library(testthat)
 
-testthat("clean_workforce_data normalizes schema", {
+test_that("clean_workforce_data normalizes schema", {
     path <- test_path("testdata", "23621-0008_de_flat.csv")
     df <- load_workforce_data(path)
 
     cleaned_df <- clean_workforce_data(df)
 
     # Check that the cleaned data frame has the expected columns
-    expected_columns <- c("year", "age_group", "profession_code", "profession_label", "fte",
-                          "fte_quality")
+    expected_columns <- c("year", "age_group", "profession_code", "profession_label", "fte")
     expect_setequal(colnames(cleaned_df), expected_columns)
     expect_true(is.integer(cleaned_df$year))
     expect_true(is.numeric(cleaned_df$fte))
-})
+    })
 
 
