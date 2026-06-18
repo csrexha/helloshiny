7a8f6447c981056dc38ca4cc69618eb8e9ad064a
HaCSRex <hachungshingrex@gmail.com>
2026-06-18 15:01:38 +0200

    ci: update documentation for clean_workforce_data function

--- R/clean_workforce_data.r
+++ R/clean_workforce_data.r
@@ -1,3 +1,9 @@
+#' Clean workforce data
+#'
+#' @param df A data frame
+#' @return A cleaned data frame
+#' @export
+ 
 clean_workforce_data <- function(data) {
     selected_columns <- c(year="time", 
                           age_group="2_variable_attribute_label", 
@@ -6,8 +12,8 @@ clean_workforce_data <- function(data) {
                           fte="value")
 
     cleaned_data <- data  |>
-        dplyr::select(all_of(selected_columns)) |>
-        mutate(year = as.integer(year),
+        select(all_of(selected_columns)) |>
+        mutate(year = as.integer(year), 
                fte = as.numeric(fte))
 
     return(cleaned_data)
