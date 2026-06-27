b767fab6594b9405dbf724ba5ced882c9e8500da
HaCSRex <hachungshingrex@gmail.com>
2026-06-27 17:19:51 +0200

    feat: rename function parameters and add sorting function for the result before output

--- tests/testthat/age_distribution.R
+++ tests/testthat/age_distribution.R
@@ -1,12 +1,12 @@
 #' Slice profession and year
 #'
-#' @param df A data frame
+#' @param data A data frame
 #' @param profession_code profession_code
 #' @param year year
-#' @return A aggregated data frame 
+#' @return A sliced data frame 
 #' @export
 
-age_distribution <- function(df, profession_code, year) {
+age_distribution <- function(data, prof_code, time) {
     age_groups <- c(
         "unter 30 Jahre",
         "30 bis unter 40 Jahre",
@@ -15,10 +15,11 @@ age_distribution <- function(df, profession_code, year) {
         "60 Jahre und mehr"
     )
 
-    sliced_df <- df |>
-        filter(profession_code==profession_code & year==year)|>
-        filter(age_group != "Insgesamt") |>     # remove extra age group
-        mutate(age_group = factor(age_group, levels=age_groups, ordered=TRUE))  # reorder the age group
+    sliced_df <- data |>
+        dplyr::filter(profession_code==prof_code & year==time) |>
+        dplyr::filter(age_group != "Insgesamt") |>     # remove extra age group
+        mutate(age_group = factor(age_group, levels=age_groups)) |> # reorder the age group
+        arrange(age_group)
     
     return(sliced_df)
 }
